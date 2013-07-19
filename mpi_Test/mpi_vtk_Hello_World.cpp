d3a9b12f4e06ec80171244d344e7041df0f3eb34
root <neto@caltech.edu>
2013-07-19 16:20:02 -0400

    Opens the html file on local server

--- mpi_Test/mpi_vtk_Hello_World.cpp
+++ mpi_Test/mpi_vtk_Hello_World.cpp
@@ -1,7 +1,10 @@
+//#include <windows.h>
+
 #include "vtkMPIController.h" 
 
 void process(vtkMultiProcessController* controller, void* vtkNotUsed(arg)) 
-{ 
+{
+    system("google-chrome http://127.0.0.1/Toggle_git/vtkOBJ/build/ThreejsExampleVTK.html"); 
     int myId = controller->GetLocalProcessId(); 
 
     std::cout << "My process id is "; 
@@ -9,10 +12,11 @@ void process(vtkMultiProcessController* controller, void* vtkNotUsed(arg))
 } 
 
 int main( int argc, char* argv[] ) 
-{ 
+{   
     vtkMPIController* controller = vtkMPIController::New(); 
     controller->Initialize(&argc, &argv); 
 
+    //ShellExecute(NULL, "open", "http://dreamincode.net", NULL, NULL, SW_SHOWNORMAL);
 
     controller->SetSingleMethod(process, 0); 
     controller->SingleMethodExecute(); 
